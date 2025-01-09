4596226af6693bbbb2f8e6d283f94d1ca60acc87
BartKempa <bartekkempa@gmail.com>
2025-01-09 09:52:11 +0100

    Added filter jwtAuthenticationFilter to filterChain

--- src/main/java/com/example/apiecommerce/domain/config/SecurityConfig.java
+++ src/main/java/com/example/apiecommerce/domain/config/SecurityConfig.java
@@ -1,23 +1,30 @@
 package com.example.apiecommerce.domain.config;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 public class SecurityConfig {
+    private final JwtService jwtService;
     private static final String USER_ROLE = "USER";
     private static final String ADMIN_ROLE = "ADMIN";
+
+    public SecurityConfig(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
 
     @Bean
     MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector){
@@ -25,7 +32,11 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http,
+                                           MvcRequestMatcher.Builder mvc,
+                                           AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.getOrBuild();
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtService);
         http.authorizeHttpRequests(request -> request
                         .requestMatchers(mvc.pattern(HttpMethod.POST, "/api/v1/**")).hasRole(ADMIN_ROLE)
                         .requestMatchers(mvc.pattern(HttpMethod.PUT, "/api/v1/**")).hasRole(ADMIN_ROLE)
@@ -33,16 +44,14 @@ public class SecurityConfig {
                         .requestMatchers(mvc.pattern(HttpMethod.DELETE, "/api/v1/**")).hasRole(ADMIN_ROLE)
                         .requestMatchers(mvc.pattern(HttpMethod.GET, "/api/v1/**")).hasAnyRole(USER_ROLE, ADMIN_ROLE)
                         .anyRequest().permitAll())
-                .httpBasic(Customizer.withDefaults())
                 .sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .csrf(csrfCustomizer  -> csrfCustomizer .disable());
+                .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtAuthenticationFilter, AuthorizationFilter.class);
         return http.build();
-
     }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-
             return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
