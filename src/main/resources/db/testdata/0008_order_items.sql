95b5e9601011f35b9517265b8308df48d6053c72
BartKempa <bartekkempa@gmail.com>
2025-02-08 11:16:23 +0100

    Added test data

---
+++ src/main/resources/db/testdata/0008_order_items.sql
@@ -0,0 +1,10 @@
+INSERT INTO order_item (order_item_quantity, order_id, product_id) VALUES
+                                                                       (2, 1, 1),
+                                                                       (1, 1, 3),
+                                                                       (4, 2, 2),
+                                                                       (1, 2, 5),
+                                                                       (3, 3, 6),
+                                                                       (2, 3, 7),
+                                                                       (5, 4, 8),
+                                                                       (1, 5, 4),
+                                                                       (2, 5, 1);
---
+++ src/main/resources/db/testdata/0009_deliveries.sql
@@ -0,0 +1,5 @@
+insert into delivery (delivery_name, delivery_time, delivery_charge) values
+                                                                         ('Poczta', '3-5 dni roboczych', 10.00),
+                                                                         ('Kurier UDC', '1-2 dni robocze', 25.00),
+                                                                         ('Kurier THL', 'Kilka godzin', 40.00),
+                                                                         ('Odbiór osobisty', 'Natychmiastowy', 0.00);
